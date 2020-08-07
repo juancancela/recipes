bcb94f0b404390f3224a6a63dc4f2a4a53fda51c
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2020-08-07 00:56:23 -0300

    added more test items

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,11 +8,13 @@ const items = [
   {title: 'auto2', desc: 'auto', id: '2'},
   {title: 'moto1', desc: 'moto', id: '3'},
   {title: 'moto2', desc: 'moto', id: '4'},
-  {title: 'moto3', desc: 'moto', id: '3'},
-  {title: 'moto4', desc: 'moto', id: '4'},
-  {title: 'auto3', desc: 'auto', id: '3'},
-  {title: 'auto4', desc: 'auto', id: '4'},
-  {title: 'ski1', desc: 'ski', id: '4'},
+  {title: 'moto3', desc: 'moto', id: '5'},
+  {title: 'moto4', desc: 'moto', id: '6'},
+  {title: 'auto3', desc: 'auto', id: '7'},
+  {title: 'auto4', desc: 'auto', id: '8'},
+  {title: 'ski1', desc: 'ski', id: '9'},
+  {title: 'ski2', desc: 'ski', id: '10'},
+  {title: 'ski3', desc: 'ski', id: '11'},
 ];
 
 export const HomePage = React.memo(() => {
